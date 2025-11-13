8828c67f643f03642528f2e24323d4fd060498dd
malshapraboth <malshapraboth@gmail.com>
2025-11-13 14:07:00 +0100

    finished ex02

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -10,4 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cstdlib>
+#include <iostream>
 
+int	main(void)
+{
+	std::string string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+    std::cout << "Memory address of String:    " << &string << std::endl;
+    std::cout << "Memory address of StringPTR: " << stringPTR << std::endl;
+    std::cout << "Memory address of StringREF: " << &stringREF << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Value of String:             " << string << std::endl;
+    std::cout << "Value of StringPTR:          " << *stringPTR << std::endl;
+    std::cout << "Value of StringREF:          " << stringREF << std::endl;
+
+	return (EXIT_SUCCESS);
+}

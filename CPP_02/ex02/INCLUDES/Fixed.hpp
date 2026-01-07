04d9780533d2e554445d2bc65dde4da6c680501c
malshapraboth <malshapraboth@gmail.com>
2026-01-07 11:29:08 +0100

    added nothing

--- CPP_02/ex02/INCLUDES/Fixed.hpp
+++ CPP_02/ex02/INCLUDES/Fixed.hpp
@@ -3,26 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: malshapraboth <marvin@42.fr>               +#+  +:+       +#+        */
+/*   By: kadferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/15 12:27:22 by malshapraboth     #+#    #+#             */
-/*   Updated: 2025/12/15 12:27:23 by malshapraboth    ###   ########.fr       */
+/*   Created: 2026/01/07 11:07:06 by kadferna          #+#    #+#             */
+/*   Updated: 2026/01/07 11:07:07 by kadferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <iostream>
 # include <cstdlib>
 
-
-class Fixed {
-    private:
-        int numValue;
-        static const int bits = 8;
-    public:
-        Fixed();
-        Fixed(const Fixed &other);
-        Fixed& operator=(const Fixed &other);
-        ~Fixed();
-        int getRawBits( void ) const;
-        void setRawBits( int const raw );
-};

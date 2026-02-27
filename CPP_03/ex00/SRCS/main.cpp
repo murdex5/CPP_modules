7e94c8342cb7e884b65a857e8c6a5e6fdd6c61c5
malshapraboth <kadferna@student.42roma.it>
2026-02-27 11:04:08 +0100

    adding my custom tests

--- CPP_03/ex00/SRCS/main.cpp
+++ CPP_03/ex00/SRCS/main.cpp
@@ -12,6 +12,7 @@
 
 # include "../INCLUDES/ClapTrap.hpp"
 
+
 int main( void )
 {
     ClapTrap a("Malsha");
@@ -21,3 +22,36 @@ int main( void )
     a.beRepaired(2);
     return 0;
 }
+
+
+// TEsts
+/*
+int main( void )
+{
+    ClapTrap a("Alpha");
+    ClapTrap b("Bravo");
+    ClapTrap c(a);
+    ClapTrap d("Delta");
+    d = b;
+
+    std::cout << "Testing Basic Combat & Repair" << std::endl;
+    a.attack("Bravo"); // Consumes 1 energy
+    b.takeDamage(5);   // Bravo takes 5 damage
+    b.beRepaired(3);   // Bravo repairs 3 HP (consumes 1 energy)
+
+    std::cout << "\nTesting Energy." << std::endl;
+    // 'a' started with 10 energy, used 1. Let's use the remaining 9.
+    for (int i = 0; i < 10; i++) {
+        a.attack("a wooden dummy");
+    }
+    // Now 'a' should be out of energy and fail to repair
+    a.beRepaired(5);
+
+    std::cout << "Testing Death (0 Hit Points)" << std::endl;
+    c.takeDamage(10); // 'c' (copy of Alpha) takes 10 damage, dropping to 0 HP
+    c.takeDamage(5);  // Should notify that it can't take more damage
+    c.attack("Bravo"); // Should fail because of 0 HP
+
+    return 0;
+}
+    */
